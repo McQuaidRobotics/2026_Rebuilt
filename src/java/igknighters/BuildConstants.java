8a40f770fe7eb92761750c5984bee4371862b01b
Henry <henryj.winchester@outlook.com>
2026-01-13 09:13:00 -0500

    update the buildconstants

--- src/java/igknighters/BuildConstants.java
+++ src/java/igknighters/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026_Rebuilt";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 9;
-    public static final String GIT_SHA = "caf31596378f6ba34a0b6419ecfa37a731eb0830";
-    public static final String GIT_DATE = "2026-01-12 20:37:52 EST";
+    public static final int GIT_REVISION = 10;
+    public static final String GIT_SHA = "6878d998de56b175b7e896d712a1ce876dbe418d";
+    public static final String GIT_DATE = "2026-01-13 09:11:18 EST";
     public static final String GIT_BRANCH = "example-subsystems-to-act-as-examples";
-    public static final String BUILD_DATE = "2026-01-12 20:40:49 EST";
-    public static final long BUILD_UNIX_TIME = 1768268449112L;
+    public static final String BUILD_DATE = "2026-01-13 09:11:20 EST";
+    public static final long BUILD_UNIX_TIME = 1768313480602L;
     public static final int DIRTY = 0;
 
     private BuildConstants() {}
