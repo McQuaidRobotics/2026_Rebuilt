841930afcbeff91120b5b2500a63b166d789252e
Henry <henryj.winchester@outlook.com>
2026-01-12 19:21:54 -0500

    got rid of spamming prints somethings wrong witht the api + ctre

--- src/java/igknighters/BuildConstants.java
+++ src/java/igknighters/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026_Rebuilt";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 8;
-    public static final String GIT_SHA = "336fc336f425564823fbe599d790d5fa24f7ada5";
-    public static final String GIT_DATE = "2026-01-11 16:47:11 EST";
+    public static final int GIT_REVISION = 9;
+    public static final String GIT_SHA = "077dddaca0bfb34fb67b7db9eaf5dd2826439476";
+    public static final String GIT_DATE = "2026-01-12 18:41:13 EST";
     public static final String GIT_BRANCH = "Luma-for-real";
-    public static final String BUILD_DATE = "2026-01-12 18:40:56 EST";
-    public static final long BUILD_UNIX_TIME = 1768261256969L;
+    public static final String BUILD_DATE = "2026-01-12 19:13:47 EST";
+    public static final long BUILD_UNIX_TIME = 1768263227587L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
