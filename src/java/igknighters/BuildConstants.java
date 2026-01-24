4fec2b6528e46bc88613332287fa8304c3ec6b9d
Henry <henryj.winchester@outlook.com>
2026-01-24 15:41:29 -0500

    idk

--- src/java/igknighters/BuildConstants.java
+++ src/java/igknighters/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026_Rebuilt";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 45;
-    public static final String GIT_SHA = "0f7d3d11e34845895f686539e1a6c7c403ce3e1d";
-    public static final String GIT_DATE = "2026-01-23 12:57:18 EST";
+    public static final int GIT_REVISION = 46;
+    public static final String GIT_SHA = "36bd61c342a460c5a205126c0133aba663e2a65a";
+    public static final String GIT_DATE = "2026-01-24 12:16:50 EST";
     public static final String GIT_BRANCH = "Climber";
-    public static final String BUILD_DATE = "2026-01-24 11:31:12 EST";
-    public static final long BUILD_UNIX_TIME = 1769272272817L;
+    public static final String BUILD_DATE = "2026-01-24 14:04:50 EST";
+    public static final long BUILD_UNIX_TIME = 1769281490189L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
--- src/java/igknighters/Robot.java
+++ src/java/igknighters/Robot.java
@@ -172,7 +172,7 @@ public class Robot extends TimedRobot {
     }
 
     public void bindDriverController() {
-        driverController.bind(subsytems, DriverController.DebugType.CLIMBER);
+        driverController.bind(subsytems, DriverController.DebugType.SHOOTER);
     }
 
     @Override
