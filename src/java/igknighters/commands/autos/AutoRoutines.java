c545423b8d5e00a386ff815e1d50742868fddf1d
Cam <cevans29@student.mcquaid.org>
2026-01-27 17:40:26 -0500

    center outpost climp partial

--- src/java/igknighters/commands/autos/AutoRoutines.java
+++ src/java/igknighters/commands/autos/AutoRoutines.java
@@ -151,6 +151,8 @@ public class AutoRoutines extends AutoCommands {
                                                 HigherOrderCommands.shootNoStop(subsystems),
                                                 moveTraj.cmd()))
                                 .withName("Move and Shoot"));
+        HigherOrderCommands.prepToClimbFirstRung(subsystems);
+        moveTraj.cmd();
         moveTraj.atTimeBeforeEnd(0.0).onTrue(SwerveCommands.stopDriving(swerve));
         return routine;
     }
