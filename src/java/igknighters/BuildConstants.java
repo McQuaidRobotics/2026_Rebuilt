acb7907e6c7fe34d44f9807baefad6b88253d374
Cameron <cevans29@student.mcquaid.org>
2026-01-30 19:06:00 -0500

    fix

--- src/java/igknighters/BuildConstants.java
+++ src/java/igknighters/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026_Rebuilt";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 60;
-    public static final String GIT_SHA = "a8b7ab2c7857e5a36a224e71d0eb72a322373229";
-    public static final String GIT_DATE = "2026-01-27 19:48:07 EST";
+    public static final int GIT_REVISION = 61;
+    public static final String GIT_SHA = "be3009b4ec17b77306673b9d7255e630130c3bd8";
+    public static final String GIT_DATE = "2026-01-27 20:39:14 EST";
     public static final String GIT_BRANCH = "Autos---Cameron";
-    public static final String BUILD_DATE = "2026-01-27 20:33:14 EST";
-    public static final long BUILD_UNIX_TIME = 1769563994406L;
+    public static final String BUILD_DATE = "2026-01-30 18:58:02 EST";
+    public static final long BUILD_UNIX_TIME = 1769817482739L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
