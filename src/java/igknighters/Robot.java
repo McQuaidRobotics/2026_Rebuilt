12daa41dc44d8079c7f62968ea43eb6cf46aeb1d
James <jtacci28@student.mcquaid.org>
2026-02-07 16:50:04 -0500

    aaa

--- src/java/igknighters/Robot.java
+++ src/java/igknighters/Robot.java
@@ -160,7 +160,6 @@ public class Robot extends TimedRobot {
         DogLog.log(
                 "Subsystems/Vision/ObjectDetection/Closest Game Piece",
                 subsytems.luma.getClosestGamePiece());
-        DogLog.log("Subsystems/Vision/ObjectDetection/number one", "im 1ing it");
 
         if (kUseLimelight) {
             var driveState = subsytems.swerve.getState();
