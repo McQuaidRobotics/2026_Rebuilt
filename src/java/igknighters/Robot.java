280cbe7e64a08f82d86c22e268c081033783784a
Henry <henryj.winchester@outlook.com>
2026-02-13 20:28:34 -0500

    its cool. Shoot on move misses but rest is sick af

--- src/java/igknighters/Robot.java
+++ src/java/igknighters/Robot.java
@@ -19,7 +19,6 @@ import edu.wpi.first.units.measure.*;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
