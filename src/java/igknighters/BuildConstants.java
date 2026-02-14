345dfa7357c3704b2a3579421005fbff878aaedf
Elcarman <Elliotcarman8@icloud.com>
2026-02-14 15:20:02 -0500

    getting started

--- src/java/igknighters/BuildConstants.java
+++ src/java/igknighters/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026_Rebuilt";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 47;
-    public static final String GIT_SHA = "fb46339989158dbd16c6d755ebd94f0af033f2b1";
-    public static final String GIT_DATE = "2026-02-12 20:52:36 EST";
-    public static final String GIT_BRANCH = "wayfinder-v2";
-    public static final String BUILD_DATE = "2026-02-14 12:23:49 EST";
-    public static final long BUILD_UNIX_TIME = 1771089829686L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 50;
+    public static final String GIT_SHA = "4016846e00381eae2b11c89a66e3659470aaeef4";
+    public static final String GIT_DATE = "2026-02-14 14:15:21 EST";
+    public static final String GIT_BRANCH = "intake-sim";
+    public static final String BUILD_DATE = "2026-02-14 15:12:48 EST";
+    public static final long BUILD_UNIX_TIME = 1771099968059L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
---
+++ src/java/igknighters/subsystems/intake/IntakeVisualizer.java
@@ -0,0 +1,98 @@
+package igknighters.subsystems.intake;
+
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+
+public class IntakeVisualizer {
+    private final Mechanism2d intake = new Mechanism2d(1, 1);
+
+    private final MechanismRoot2d pivot = intake.getRoot("pivot", 0.5, 0.0);
+
+    private final double PIVOT_LENGTH = 0.3;
+
+    private final double PIVOT_WIDTH = 30;
+
+    private final double ROBOT_LENGTH = .6;
+
+    private final Color8Bit RobotColor = new Color8Bit(Color.kAliceBlue);
+    private final Color8Bit frontRobot = new Color8Bit(Color.kBeige);
+
+    private final MechanismLigament2d robotBridge =
+            pivot.append(
+                    new MechanismLigament2d(
+                            "bridge", ROBOT_LENGTH / 2, 0.0, 0.0, new Color8Bit(Color.kBlack)));
+    private final MechanismLigament2d bridgeToTop =
+            robotBridge.append(
+                    new MechanismLigament2d(
+                            "bridge-to-top",
+                            ROBOT_LENGTH / 2,
+                            90.0,
+                            5,
+                            new Color8Bit(Color.kYellow)));
+    private final MechanismLigament2d topRtoTopL =
+            bridgeToTop.append(
+                    new MechanismLigament2d(
+                            "top-to-right", ROBOT_LENGTH, 90.0, 5, new Color8Bit(Color.kAqua)));
+    private final MechanismLigament2d topLToBottomL =
+            topRtoTopL.append(
+                    new MechanismLigament2d(
+                            "topL-to-bottomL",
+                            ROBOT_LENGTH,
+                            90.0,
+                            5,
+                            new Color8Bit(Color.kViolet)));
+    private final MechanismLigament2d bottomLToBottomR =
+            topLToBottomL.append(
+                    new MechanismLigament2d(
+                            "botomL-to-right", ROBOT_LENGTH, 90.0, 5, new Color8Bit(Color.kAqua)));
+    private final MechanismLigament2d bottomToBridge =
+            bottomLToBottomR.append(
+                    new MechanismLigament2d(
+                            "bottom-to-bridge",
+                            ROBOT_LENGTH / 2,
+                            90.0,
+                            5,
+                            new Color8Bit(Color.kYellow)));
+
+    private final MechanismLigament2d pivotLigament =
+            pivot.append(
+                    new MechanismLigament2d(
+                            "PIVOT_LIGAMENT",
+                            PIVOT_LENGTH,
+                            0.0,
+                            PIVOT_WIDTH,
+                            new Color8Bit(Color.kRed)));
+
+    public IntakeVisualizer() {
+        intake.setBackgroundColor(new Color8Bit(Color.kBlack));
+
+        SmartDashboard.putData("Intake Visualizer", intake);
+    }
+
+    // public Color8Bit getRPMColor(double rpm) {
+
+    //     if (targetRPM <= 0.0) {
+    //         return new Color8Bit(255, 0, 0);
+    //     }
+    //     double ratio = rpm / targetRPM;
+
+    //     ratio = Math.min(ratio, 1.0);
+
+    //     double g = 255.0 * Math.min(ratio, 1.0);
+    //     double r = 255.0 * (1.0 - ratio);
+
+    //     return new Color8Bit((int) r, (int) g, 0);
+    // }
+
+    public void update(IntakeState intakeState) {
+        double hoodAngleDegrees = Math.toDegrees(intakeState.pivotDegrees);
+        double rpm = intakeState.rollerSpeedRPM;
+
+        pivotLigament.setAngle(intakeState.pivotDegrees);
+        // pivotLigament.setColor(getRPMColor(intakeState.rollerSpeedRPM));
+    }
+}
