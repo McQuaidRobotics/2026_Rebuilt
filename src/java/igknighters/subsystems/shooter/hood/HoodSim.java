c3605fbae957c387356f8b7515f81c408daf325d
Henry <henryj.winchester@outlook.com>
2026-02-18 20:04:40 -0500

    hood is officially bsed enough we pass

--- src/java/igknighters/subsystems/shooter/hood/HoodSim.java
+++ src/java/igknighters/subsystems/shooter/hood/HoodSim.java
@@ -13,11 +13,9 @@ public class HoodSim extends Hood {
     private final SingleJointedArmSim flapSim =
             new SingleJointedArmSim(
                     LinearSystemId.createSingleJointedArmSystem(
-                            DCMotor.getKrakenX60(1),
-                            SubsystemConstants.kShooter.kHood.JKG_M2,
-                            SubsystemConstants.kShooter.kHood.MOTOR_ROTS_TO_HOOD_DEGREES),
+                            DCMotor.getKrakenX60(1), SubsystemConstants.kShooter.kHood.JKG_M2, 1.0),
                     DCMotor.getKrakenX60(1),
-                    SubsystemConstants.kShooter.kHood.MOTOR_ROTS_TO_HOOD_DEGREES,
+                    1.0,
                     SubsystemConstants.kShooter.kHood.LENGTH_METERS,
                     Math.toRadians(SubsystemConstants.kShooter.kHood.MIN_ANGLE_DEGREES),
                     Math.toRadians(SubsystemConstants.kShooter.kHood.MAX_ANGLE_DEGREES),
@@ -25,7 +23,13 @@ public class HoodSim extends Hood {
                     Math.toRadians(SubsystemConstants.kShooter.kHood.MIN_ANGLE_DEGREES));
 
     private final ProfiledPIDController pidController =
-            new ProfiledPIDController(.01, 0.0, 0.0, new TrapezoidProfile.Constraints(2, 3));
+            new ProfiledPIDController(
+                    SubsystemConstants.kShooter.kHood.kP,
+                    SubsystemConstants.kShooter.kHood.kI,
+                    SubsystemConstants.kShooter.kHood.kD,
+                    new TrapezoidProfile.Constraints(
+                            SubsystemConstants.kShooter.kHood.MAX_SPEED_R_P_S,
+                            SubsystemConstants.kShooter.kHood.MAX_ACCEL_R_P_S_S));
 
     private boolean isControlledThisCycle = false;
 
