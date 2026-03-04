b02783c836fbadf8716a57021853f89ffcdf9466
Elcarman <Elliotcarman8@icloud.com>
2026-03-03 19:11:27 -0500

    i don't understand what is happening

--- src/java/igknighters/commands/SubsystemTriggers.java
+++ src/java/igknighters/commands/SubsystemTriggers.java
@@ -73,7 +73,6 @@ public class SubsystemTriggers {
         passTrigger.whileTrue(
                 HigherOrderCommands.fireAtTarget(
                         subsystems, getPoseFromString("robot/passWaypoint")));
-                        subsystems, getPoseFromString("robot/passWaypoint")));
         moveToTrigger.whileTrue(
                 Repulsor.moveWithRepulsor(
                         swerve, getPoseFromString("robot/moveWaypoint").toPose2d()));
