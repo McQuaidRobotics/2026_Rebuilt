b50538a9f3db358eb1926ea97b7c24de4e3814a3
Henry <henryj.winchester@outlook.com>
2026-03-06 08:41:16 -0500

    we holdin

--- src/java/igknighters/controllers/DriverController.java
+++ src/java/igknighters/controllers/DriverController.java
@@ -187,7 +187,7 @@ public class DriverController {
         this.DPR.whileTrue(HigherOrderCommands.prepToClimbFirstRung(subsystems));
         this.X.whileTrue(ClimberCommands.holdAtState(subsystems.climber, ClimberState.LATCH_ON));
         this.DPL.whileTrue(ClimberCommands.holdAtState(subsystems.climber, ClimberState.STOW));
-        this.DPU.whileTrue(ClimberCommands.goToState(subsystems.climber, ClimberState.PULL_UP));
+        this.DPU.whileTrue(ClimberCommands.holdAtState(subsystems.climber, ClimberState.PULL_UP));
     }
 
     private DoubleSupplier deadbandSupplier(DoubleSupplier supplier, double deadband) {
