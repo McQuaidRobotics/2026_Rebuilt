557300d2f7f1911c8a1e93138eff680db934708e
Henry <henryj.winchester@outlook.com>
2026-03-09 17:50:33 -0400

    autos

--- src/java/igknighters/subsystems/shooter/AimSolver.java
+++ src/java/igknighters/subsystems/shooter/AimSolver.java
@@ -280,7 +280,7 @@ public class AimSolver {
 
             // 3. Final Angles
             double absoluteFieldAngle = Math.atan2(dy, dx);
-            double robotYawFuture = predictedPose.getRotation().getRadians();
+            double robotYawFuture = shooterPose.getRotation().getZ();
             double turretAngle =
                     Math.atan2(
                             Math.sin(absoluteFieldAngle - robotYawFuture),
@@ -412,8 +412,7 @@ public class AimSolver {
 
             // 3. Final Angles
             double absoluteFieldAngle = Math.atan2(dy, dx);
-            double robotYawFuture =
-                    predictedPose.getRotation().getRadians();
+            double robotYawFuture = shooterPose.getRotation().getZ();
             double turretAngle =
                     Math.atan2(
                             Math.sin(absoluteFieldAngle - robotYawFuture),
