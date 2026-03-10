b8929ee0c92fe0c728735d89e44d4d46dd40272d
Elcarman <Elliotcarman8@icloud.com>
2026-03-10 17:57:48 -0400

    made it pretty and used chassispeeds instead of lists of doubles

--- src/java/igknighters/util/log/Log.java
+++ src/java/igknighters/util/log/Log.java
@@ -57,6 +57,10 @@ public class Log {
         Logger.recordOutput(path, value);
     }
 
+    public static void log(String path, ChassisSpeeds[] value) {
+        Logger.recordOutput(path, value);
+    }
+
     public static void log(String path, boolean value) {
         Logger.recordOutput(path, value);
     }
