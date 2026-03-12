6d89a1ca7d68c24b4c02a340848f48fb2a943599
Henry <henryj.winchester@outlook.com>
2026-03-12 14:45:41 -0400

    new const radius

--- src/java/igknighters/subsystems/swerve/swerveconstants/GeminiConsts.java
+++ src/java/igknighters/subsystems/swerve/swerveconstants/GeminiConsts.java
@@ -94,7 +94,7 @@ public class GeminiConsts extends CommonSwerveConsts {
 
     private static final double kDriveGearRatio = 5.902777777777778;
     private static final double kSteerGearRatio = 21.428571428571427;
-    private static final Distance kWheelRadius = Inches.of(2);
+    private static final Distance kWheelRadius = Inches.of(2.5);
 
     private static final boolean kInvertLeftSide = false;
     private static final boolean kInvertRightSide = true;
