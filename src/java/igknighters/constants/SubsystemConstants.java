1d798a21a2ea69db0447f1cd80224ac6fa28a7b3
Henry <henryj.winchester@outlook.com>
2026-03-13 07:44:45 -0400

    enable pivot

--- src/java/igknighters/constants/SubsystemConstants.java
+++ src/java/igknighters/constants/SubsystemConstants.java
@@ -158,7 +158,7 @@ public class SubsystemConstants {
             public static final double MIN_ANGLE_DEGREES = 0.0;
             public static final double MAX_SPEED_METERS_PER_SECOND = 1.0;
             public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = .5;
-            public static final double ENCODER_OFFSET = -0.082275390625;
+            public static final double ENCODER_OFFSET = 0.075927734375;
             public static final double MAX_JERK = 1.0;
             public static final int STATOR_CURRENT_LIMIT = 20;
             public static final int SUPPLY_CURRENT_LIMIT = 20;
