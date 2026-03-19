24ee8cf778746638207b23e17d55060844416699
Elcarman <Elliotcarman8@icloud.com>
2026-03-19 13:58:58 -0400

    got started

---
+++ src/java/igknighters/commands/Wayfinder.java
@@ -0,0 +1,47 @@
+package igknighters.commands;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import igknighters.commands.Repulsor.obstacle;
+import igknighters.constants.FieldConstants;
+
+public class Wayfinder {
+    
+    public Pose2d[] wayfind(Pose2d currentPose, Pose2d goalPose)
+    {
+        Pose2d[] path = new Pose2d[1];
+        
+        ArrayList<obstacle> obstacles = FieldConstants.OBSTACLES.ALL_OBSTACLES;
+
+        //the rotational step distance when searching for path (degrees)
+        double omegaStep = 15;
+
+        //the translational step distance when searching for path (meters)
+        double translationalStep = .1;
+        
+        do {
+            path[0] = currentPose;
+            for (obstacle obs:obstacles)
+            {
+
+            }
+
+        } while (!goalState(path[-1], goalPose));
+
+
+        return path;
+    }
+
+    //check if at goal state with 0.5 meter tolerance
+    public boolean goalState(Pose2d lastPathPose, Pose2d goalPose)
+    {
+        double tolerance = 0.5;
+        if (Math.abs(goalPose.getX() - lastPathPose.getX()) < tolerance && Math.abs(goalPose.getY()-lastPathPose.getY()) < tolerance)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+}
