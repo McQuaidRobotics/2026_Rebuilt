0303074c93f0c8a083054ea0474284906f4531ed
Cameron E <cevans29@student.mcquaid.org>
2026-03-27 17:39:43 -0400

    choreo update

--- src/java/igknighters/commands/autos/AutoRoutines.java
+++ src/java/igknighters/commands/autos/AutoRoutines.java
@@ -385,7 +385,7 @@ public class AutoRoutines extends AutoCommands {
         swipe1In.done()
                 .onTrue(
                         SwerveCommands.stopDriving(swerve)
-                                .alongWith(HigherOrderCommands.shootTillEmpty(subsystems, 2))
+                                .alongWith(HigherOrderCommands.shootTillEmpty(subsystems, 1))
                                 .andThen(swipe2Out.cmd()));
 
         swipe2Out.active().onTrue(IntakeCommands.holdAtIntake(subsystems.intake));
