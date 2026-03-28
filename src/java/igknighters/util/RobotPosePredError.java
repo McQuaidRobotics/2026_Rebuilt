dc1f5db635388edb6a4f628a208fda51214b30cc
Elcarman <Elliotcarman8@icloud.com>
2026-03-28 10:21:30 -0400

    boutta test

--- src/java/igknighters/util/RobotPosePredError.java
+++ src/java/igknighters/util/RobotPosePredError.java
@@ -1,24 +1,26 @@
-package igknighters.util;
+// package igknighters.util;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import igknighters.Robot;
+// import edu.wpi.first.math.geometry.Pose2d;
+// import igknighters.Robot;
+// import igknighters.util.log.Log;
 
-public class RobotPosePredError {
-    Pose2d[] predictionHistory = new Pose2d[2];
-    int writeIndex = 0;
-    public void logPose(Pose2d currentPose)
-    {
-        predictionHistory[writeIndex] = Robot.pose_pred.getPredictedPose(currentPose);
-        if (writeIndex != 1) {
-            writeIndex++;
-        }
-        else {
-            writeIndex = 0;
-        }
+// public class RobotPosePredError {
+//     Pose2d[] predictionHistory = new Pose2d[2];
+//     int writeIndex = 0;
+//     public void logPose(Pose2d currentPose)
+//     {
+//         predictionHistory[writeIndex] = Robot.pose_pred.getPredictedPose(currentPose);
+//         if (writeIndex != 1) {
+//             writeIndex++;
+//         }
+//         else {
+//             writeIndex = 0;
+//         }
+//     }
 
-    }
+//     Log.Log(predictionHistory[0]-predictionHistory[1]);
 
-    public double findError() {
+//     public double findError() {
 
-    }
-}
+//     }
+// }
