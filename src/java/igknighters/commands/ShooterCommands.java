bdba6ef6942682b95cc7f1a825531b656dfd4742
Cameron E <cevans29@student.mcquaid.org>
2026-03-30 14:54:52 -0400

    hopeful merge conflict fix

--- src/java/igknighters/commands/ShooterCommands.java
+++ src/java/igknighters/commands/ShooterCommands.java
@@ -179,8 +179,9 @@ public class ShooterCommands {
     }
 
     public static BooleanSupplier isUnderTrench(Supplier<Pose2d> robotPoseSupplier) {
-        Supplier<Pose2d> turretPose = getShooterPoseWithOffset(robotPoseSupplier);
+                Supplier<Pose2d> turretPose = getShooterPoseWithOffset(robotPoseSupplier);
         return () -> {
+                
             boolean under1 =
                     isBetween(
                             turretPose.get(),
