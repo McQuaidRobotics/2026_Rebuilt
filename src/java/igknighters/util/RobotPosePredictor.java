3b934a91e270a4442e7d2e4a1f145aae4db76348
Elcarman <Elliotcarman8@icloud.com>
2026-04-04 16:46:37 -0400

    niwo

--- src/java/igknighters/util/RobotPosePredictor.java
+++ src/java/igknighters/util/RobotPosePredictor.java
@@ -134,8 +134,8 @@ public class RobotPosePredictor {
         // handle wrapping
         double predOmega =
                 currentPose[2]
-                        + veloHistory[latestIdx].omegaRadiansPerSecond * 0.3
-                        + 1 / 2 * predictedRotAcc * Math.pow(0.3, 2);
+                        + veloHistory[latestIdx].omegaRadiansPerSecond * 0.1
+                        + 1 / 2 * predictedRotAcc * Math.pow(0.1, 2);
 
         if (predOmega > Math.PI) {
             predicted[2] = predOmega - 2 * Math.PI;
