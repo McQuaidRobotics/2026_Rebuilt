74af41536bae9819b291bb84cda335ea66ee8c43
James <jtacci28@student.mcquaid.org>
2026-04-04 16:50:20 -0400

    rah

--- src/java/igknighters/Robot.java
+++ src/java/igknighters/Robot.java
@@ -29,7 +29,6 @@ import igknighters.constants.DrivingSharedState;
 import igknighters.constants.SubsystemConstants;
 import igknighters.constants.SubsystemConstants.kShooter.kFlywheels;
 import igknighters.controllers.DriverController;
-import igknighters.controllers.DriverController.DebugType;
 import igknighters.subsystems.LimeLightVision.LimeLightVision;
 import igknighters.subsystems.Luma.Luma;
 import igknighters.subsystems.Subsystems;
