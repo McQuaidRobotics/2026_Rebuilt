94499dc5d7e9c62db0c605b6727d2f300456b294
Henry <henryj.winchester@outlook.com>
2026-04-08 19:26:51 -0400

    idk

--- src/java/igknighters/subsystems/LimeLightVision/Cameras/LimeLightVisionReal.java
+++ src/java/igknighters/subsystems/LimeLightVision/Cameras/LimeLightVisionReal.java
@@ -55,8 +55,9 @@ public class LimeLightVisionReal extends LimeLights {
                 // --- ROTATION SELECTION LOGIC ---
                 Rotation2d rotationToUse;
                 if (mt1Estimate.tagCount >= 2) {
-                    if (RobotController.getFPGATime() - previousSampleTime > 0.0){
-                        // only accept the newer ones. This makes it so that if the last camera is behind the others it will still work
+                    if (RobotController.getFPGATime() - previousSampleTime > 0.0) {
+                        // only accept the newer ones. This makes it so that if the last camera is
+                        // behind the others it will still work
                         previousSampleTime = RobotController.getFPGATime();
                     }
                     rotationToUse = mt1Estimate.pose.getRotation(); // vision rotation
@@ -118,7 +119,8 @@ public class LimeLightVisionReal extends LimeLights {
     }
 
     public double timeSinceLastSample() {
-        return (RobotController.getFPGATime() - previousSampleTime) * (1/ 1000000.0); // microseconds to seconds
+        return (RobotController.getFPGATime() - previousSampleTime)
+                * (1 / 1000000.0); // microseconds to seconds
     }
 
     /** Returns the last timestamp from vision measurements. */
