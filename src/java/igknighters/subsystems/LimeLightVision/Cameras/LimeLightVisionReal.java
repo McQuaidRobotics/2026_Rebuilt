bae5020ce33c4bbad060f079e356ed1f2b2c2587
Henry <henryj.winchester@outlook.com>
2026-04-08 13:41:27 -0400

    much pretier

--- src/java/igknighters/subsystems/LimeLightVision/Cameras/LimeLightVisionReal.java
+++ src/java/igknighters/subsystems/LimeLightVision/Cameras/LimeLightVisionReal.java
@@ -4,7 +4,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import igknighters.Robot;
 import igknighters.subsystems.LimeLightVision.Helpers.LimelightHelpers;
-import igknighters.subsystems.LimeLightVision.Helpers.PoseAverager;
+import igknighters.util.Merging.PoseAverager;
 import igknighters.util.log.Log;
 import java.util.ArrayList;
 import java.util.List;
