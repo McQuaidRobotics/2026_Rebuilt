f9f9f0eaa94a08a1d0a50d05689b801e1c2faeda
Henry Winchester <132027487+HENRYTHEGR@users.noreply.github.com>
2026-04-14 13:29:22 -0400

    Remove lerp stuff

--- src/java/igknighters/constants/RobotConsts.java
+++ src/java/igknighters/constants/RobotConsts.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 import igknighters.subsystems.swerve.swerveconstants.CommonSwerveConsts;
-import igknighters.util.LerpTable;
 
 public abstract class RobotConsts {
     public abstract CANBus getSuperStructureBus();
@@ -21,8 +20,6 @@ public abstract class RobotConsts {
 
     public abstract kIntakeConsts intake();
 
-    public abstract kLerpConsts lerp();
-
     public abstract kLimelightVisionConsts limelightVision();
 
     public interface SWERVE_CONSTS {
@@ -37,40 +34,6 @@ public abstract class RobotConsts {
         kTurretConsts kTurret();
 
         kHoodConsts kHood();
-    }
-
-    public interface kLerpConsts {
-        kRPMConsts kRPM();
-
-        kHoodAngleConsts kHoodAngle();
-
-        kRadialSOTMConsts kRadialSOTM();
-
-        kTOFConsts kTimeOfFlight();
-
-        kTangentialSOTMConsts kTangentialSOTM();
-    }
-
-    public interface kTOFConsts {
-        LerpTable table();
-    }
-
-    public interface kRPMConsts {
-        LerpTable table();
-    }
-
-    public interface kHoodAngleConsts {
-        LerpTable table();
-    }
-
-    public interface kRadialSOTMConsts {
-        LerpTable away();
-
-        LerpTable towards();
-    }
-
-    public interface kTangentialSOTMConsts {
-        LerpTable table();
     }
 
     public interface kFlywheelsConsts {
