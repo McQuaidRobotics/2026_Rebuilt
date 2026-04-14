9ed34edde9991ea98f1cc68d7a4c6902bdf6b2b3
Henry Winchester <132027487+HENRYTHEGR@users.noreply.github.com>
2026-04-14 12:40:58 -0400

    ok in consts now

--- src/java/igknighters/constants/RobotConsts.java
+++ src/java/igknighters/constants/RobotConsts.java
@@ -4,6 +4,8 @@ import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 
+import igknighters.util.LerpTable;
+
 public abstract class RobotConsts {
     public abstract CANBus getSuperStructureBus();
 
@@ -19,6 +21,8 @@ public abstract class RobotConsts {
 
     public abstract kIntakeConsts intake();
 
+    public abstract kLerpConsts lerp();
+
     public abstract kLimelightVisionConsts limelightVision();
 
     public interface kShooterConsts {
@@ -29,6 +33,30 @@ public abstract class RobotConsts {
         kTurretConsts kTurret();
 
         kHoodConsts kHood();
+    }
+
+    public interface kLerpConsts {
+        kRPMConsts kRPM();
+        kHoodAngleConsts kHoodAngle();
+        kRadialSOTMConsts kRadialSOTM();
+        kTangentialSOTMConsts kTangentialSOTM();
+    }
+
+    public interface kRPMConsts {
+        LerpTable table();
+    }
+
+    public interface kHoodAngleConsts {
+     LerpTable table();
+    }
+
+    public interface kRadialSOTMConsts {
+        LerpTable away();
+        LerpTable towards();
+    }
+
+    public interface kTangentialSOTMConsts {
+        LerpTable table();
     }
 
     public interface kFlywheelsConsts {
