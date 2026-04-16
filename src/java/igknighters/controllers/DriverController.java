1ef6764b69bcf57252a3e37d67e740c4cdf55a60
Henry Winchester <132027487+HENRYTHEGR@users.noreply.github.com>
2026-04-16 11:05:39 -0400

    remove unwanted command

--- src/java/igknighters/controllers/DriverController.java
+++ src/java/igknighters/controllers/DriverController.java
@@ -148,7 +148,7 @@ public class DriverController {
         var swerve = subsystems.swerve;
         var intake = subsystems.intake;
 
-        this.LT.onTrue(IntakeCommands.toggleHoldState(subsystems.intake));
+        this.LT.onTrue(IntakeCommands.holdAtIntake(subsystems.intake));
         this.RT
                 .whileTrue(HigherOrderCommands.rapidFireStream(subsystems))
                 .onFalse(HigherOrderCommands.IdleShooter(subsystems));
