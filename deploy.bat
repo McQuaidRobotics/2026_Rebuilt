2ec2410775a9e2032aac23e0240532730293693e
HENRY <henryj.winchester@outlook.com>
2026-06-13 10:17:57 -0400

    fully working bat script. If syntax error its good now

--- deploy.bat
+++ deploy.bat
@@ -2,13 +2,16 @@
 echo Select Target Robot:
 echo [1] Team 3173
 echo [2] Team 9999
-set /p bot="Enter choice (1 (3173) or 2 (9999)) : "
+set /p bot="Enter choice (1 or 2): "
 
 if "%bot%"=="1" (
     echo Deploying to 3173...
-    gradlew deploy -PteamNumber=3173
-) if "%bot%"=="2" (
-    echo Deploying to 9999...
-    gradlew deploy -PteamNumber=9999
+    call .\gradlew deploy -PteamNumber=3173
 )
+
+if "%bot%"=="2" (
+    echo Deploying to 9999...
+    call .\gradlew deploy -PteamNumber=9999
+)
+
 pause
