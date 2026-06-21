154a9d7fa05aebbc027c1d0eaf6da4c4b166de09
Henry <henryj.winchester@outlook.com>
2026-06-21 14:44:18 -0400

    need to supply a initial position

--- src/java/igknighters/subsystems/YamsIntake/IntakePivot.java
+++ src/java/igknighters/subsystems/YamsIntake/IntakePivot.java
@@ -37,8 +37,8 @@ public class IntakePivot extends SubsystemBase {
                             Degrees.of(Robot.consts.intake().kPivot().MIN_ANGLE_DEGREES() - 10),
                             Degrees.of(Robot.consts.intake().kPivot().MAX_ANGLE_DEGREES() + 10))
                     // Starting position is where your arm starts
-                    // .withSimStartingPosition(
-                    //         Degrees.of(Robot.consts.intake().kPivot().STOWED_ANGLE_DEGREES()))
+                    .withSimStartingPosition(
+                            Degrees.of(Robot.consts.intake().kPivot().STOWED_ANGLE_DEGREES()))
                     // Telemetry name and verbosity for the arm.
                     .withTelemetry("PivotArm", TelemetryVerbosity.HIGH);
 
