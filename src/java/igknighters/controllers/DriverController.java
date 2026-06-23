82b4d1c0bddcb2db0b3d0fe0224448fdce4de049
aqua7749 <kyleh7749@gmail.com>
2026-06-23 19:49:04 -0400

    make it work

--- src/java/igknighters/controllers/DriverController.java
+++ src/java/igknighters/controllers/DriverController.java
@@ -135,7 +135,7 @@ public class DriverController {
      * @param subsystems The robot subsystems available for command targeting.
      */
     public void bind(final Subsystems subsystems) {
-        this.A.whileTrue(shooterComands.setAngle(subsystems.turret, Degrees.of(90()));
+        this.A.whileTrue(shooterComands.setAngle(subsystems.turret, Degrees.of(90)));
         // Example: this.A.whileTrue(new MyCommand(subsystems.mySubsystem));
         // Swerve driving is handled by the default command set in Robot.java,
         // so no explicit bind is needed here for basic teleop driving.
